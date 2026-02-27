f2406f066e29e1fd7e74d41a975422972954df89
Surya jith <suryajith077@gmal.com>
2026-02-27 18:22:59 +0530

    initialize

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
-import express from 'express'
-const app=express()
+import app from "./app.js"
+import connectDB from "./config/db.js"
+import dotenv, { config } from "dotenv"
+dotenv.config();
+connectDB()
+    
 
 
+
+
+
+app.listen(process.env.PORT,()=>{
+    console.log("server running")
+})
